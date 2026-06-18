9a9640550a7c7ec9ee5ee7b3ccbbfe48fa379e2e
Suelen <suelenonyrius@gmail.com>
2026-06-18 16:30:49 -0300

    adding comment

--- src/main/kotlin/com/betrybe/aula01/App.kt
+++ src/main/kotlin/com/betrybe/aula01/App.kt
@@ -1,3 +1,8 @@
+package com.betrybe.aula01
+
 fun main(){
-    
+
+    print("digite seu nome: ")
+    var nome = readln()
+    println("Hello ${nome}!")
 }
--- src/main/kotlin/com/betrybe/aula01/Ex03.kt
+++ src/main/kotlin/com/betrybe/aula01/Ex03.kt
@@ -1,2 +1,15 @@
 package com.betrybe.aula01
 
+import kotlin.math.pow
+
+
+fun main() {
+    print("Digite o seu peso: ")
+    var peso = readLine()!!.replace(",", ".").toDouble()
+    print("Digite o sua altura: ")
+    var altura = readLine()!!.replace(",", ".").toDouble()
+    print("Seu IMC é: ${calculaIMC(altura, peso)}")
+}
+// .....
+
+fun calculaIMC(altura: Double, peso: Double) = "%.2f".format(peso / (altura * altura))
